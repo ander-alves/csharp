572355df66fe101b8894f6c1e1ff932497a1d0aa
Anderson Alves <andso.alves@gmail.com>
2023-08-28 12:57:52 -0300

    Ajustando aula 05

--- aula_05/Vetores/Program.cs
+++ aula_05/Vetores/Program.cs
@@ -5,52 +5,57 @@
         static void Main(string[] args)
         {
             int[] vetor01 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
             int[] vetor02 = new int[5];
-            string[] vetor03 = new string[5];
 
-            /* for (int i = 0; i < 10; i++)
-             {
-                 Console.WriteLine($"O vetor01 com indice {i} = {vetor01[i]} ");
-             }
+            string[] vetor03 = { "Allan", "Ingrid", "João", "Gabriel", "Shomara", "Karina", "Jacqueline", "Ingrid" };
 
-             for (int i = 0; i < 5; i++)
-             {
-                 Console.Write($"Informe o Valor do Vetor02 no indice {i} = ");
-                 vetor02[i] = Convert.ToInt32(Console.ReadLine());
-             }
-
-             for (int i = 0; i < 5; i++)
-             {
-                 Console.WriteLine($"O vetor02 com indice {i} = {vetor02[i]} ");
-             }
-
-             Array.Sort( vetor02 );
-             Console.WriteLine("Vetor ordenado Com sort \n");
-
-             for (int i = 0; i < vetor02.Length; i++)
-             {
-                 Console.WriteLine($"O vetor02 com indice {i} = {vetor02[i]} ");
-             }
-
-             Array.Reverse(vetor02);
-             Console.WriteLine("Vetor ordenado Com Reverse \n");
-
-             for (int i = 0; i < vetor02.Length; i++)
-             {
-                 Console.WriteLine($"O vetor02 com indice {i} = {vetor02[i]} ");
-             }*/
-
-            for (int i = 0; i < 5; i++)
+            for (int indice = 0; indice < 10; indice++)
             {
-                Console.Write($"Informe o Valor do Vetor03 no indice {i} = ");
-                vetor03[i] = Convert.ToString(Console.ReadLine());
+                Console.WriteLine($"vetor01[{indice}] = {vetor01[indice]}");
+            }
+
+            Console.WriteLine("\n");
+
+            for (int indice = 0; indice < 5; indice++)
+            {
+                Console.Write($"vetor02[{indice}] = ");
+                vetor02[indice] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n");
+
+            Array.Sort(vetor02);
+
+            for (int indice = 0; indice < vetor02.Length; indice++)
+            {
+                Console.WriteLine($"vetor02[{indice}] = {vetor02[indice]}");
+            }
+
+            Console.WriteLine("\n");
+
+            Array.Reverse(vetor02);
+
+            for (int indice = 0; indice < vetor02.Length; indice++)
+            {
+                Console.WriteLine($"vetor02[{indice}] = {vetor02[indice]}");
             }
 
             Array.Sort(vetor03);
 
-            for (int i = 0; i < vetor03.Length; i++)
+            for (int indice = 0; indice < vetor03.Length; indice++)
             {
-                Console.WriteLine($"O vetor02 com indice {i} = {vetor03[i]} ");
+                Console.WriteLine($"vetor03[{indice}] = {vetor03[indice]}");
+            }
+
+            Console.WriteLine("Qual é a posição da Ingrid no vetor? " + Array.IndexOf(vetor03, "Ingrid"));
+
+            Console.WriteLine("Qual é a posição da Ingrid no vetor? " + Array.BinarySearch(vetor03, "Ingrid"));
+
+            for (int indice = 0; indice < vetor01.Length; indice++)
+            {
+                if (vetor01[indice] % 2 == 0)
+                    Console.WriteLine($"vetor01[{indice}] = {vetor01[indice]}");
             }
 
             Console.WriteLine("A posição 02 é igual a  " + Array.IndexOf(vetor03, 02));
