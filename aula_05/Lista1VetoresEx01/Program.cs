8f587bd21aa8a496cddbe161c569ef239b8e61ef
Anderson Alves <andso.alves@gmail.com>
2023-08-28 15:04:29 -0300

    Aula 05 - Exercicios

---
+++ aula_05/Lista1VetoresEx01/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Lista1VetoresEx01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+            int localizar;
+            int posicao = 0;
+            bool encontrado = false;
+
+            Console.WriteLine("Digite o número que você deseja encontrar: ");
+            localizar = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if (localizar == vetor[i])
+                {
+                    posicao = i;
+                    encontrado = true;
+                }
+            }
+
+            if (encontrado)
+            {
+                Console.WriteLine($"O número {localizar} está localizado na posição {posicao}");
+            }
+            else
+            {
+                Console.WriteLine($"O número {localizar} não foi encontrado!");
+            }
+
+            foreach (int i in vetor)
+            {
+                if (localizar == i)
+                {
+                    posicao = Array.IndexOf(vetor, i);
+                    encontrado = true;
+                    Console.WriteLine($"Encontrado no for each indice {posicao}");
+                    break;
+                }
+            }
+            if (!encontrado)
+
+                Console.WriteLine($"O número {localizar} não foi encontrado no foreach!");
+        }
+
+    }
+}
---
+++ aula_05/Lista2MatrizesEx03/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Lista2MatrizesEx03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int somaDiagonalPrincipal = 0;
+            int somaDiagonalSecundaria = 0;
+
+
+            Console.WriteLine("Elementos da Diagonal Principal:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(matriz[i, i] + " ");
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Elementos da Diagonal Secundária:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(matriz[i, 2 - i] + " ");
+            }
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < 3; i++)
+            {
+                somaDiagonalPrincipal += matriz[i, i];
+                somaDiagonalSecundaria += matriz[i, 2 - i];
+            }
+
+            Console.WriteLine($"Soma da Diagonal Principal: {somaDiagonalPrincipal}");
+            Console.WriteLine($"Soma da Diagonal Secundária: {somaDiagonalSecundaria}");
+        }
+    }
+}
