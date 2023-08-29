5b67b16cdd7faf15bc65ba0317a1b38193f3e0a9
Anderson Alves <andso.alves@gmail.com>
2023-08-29 17:12:45 -0300

    Atv 01 e 02

---
+++ aula_06/Atividade01Fila/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq.Expressions;
+
+namespace Atividade01Fila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<String> queue = new Queue<String>();
+            int seletor = 0;
+
+            do
+            {
+                Console.WriteLine("\nInforme a Opção desejada :\n");
+                Console.WriteLine("**********************************************\n" +
+                    "1 - ADICIONAR CLIENTES NA FILA\n" +
+                    "2 - LISTAR TODOS OS CLIENTE \n" +
+                    "3 - RETIRAR CLIENTES NA FILA\n" +
+                    "0 - SAIR\n" +
+                    "*************************************************************\n" +
+                    "Entre com a Opção desejada :\n");
+
+                seletor = Convert.ToInt32(Console.ReadLine());
+
+                switch (seletor)
+                {
+                    case 1:
+                        Console.WriteLine("\nInforme o nome Do Cliente\n");
+                        queue.Enqueue(Console.ReadLine());
+                        Console.WriteLine("\nCliente Adicionado\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("\nLista de Clientes \n");
+                        foreach (String s in queue) Console.WriteLine(s);
+                        break;
+                    case 3:
+                        if (queue.Count != 0)
+                        {
+                            Console.WriteLine("\nCliente foi removido\n");
+                            queue.Dequeue();
+                        }
+                        else
+                            Console.WriteLine("A Fila esta Vazia");
+                        break;
+                    case 0:
+                        Console.WriteLine("\nObrigado\n");
+                        break;
+                    default:
+                        Console.WriteLine("\nUnknown value\n");
+                        break;
+
+                }
+
+            } while (seletor != 0);
+
+        }
+    }
+}
---
+++ aula_06/Atividade02Pilha/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Atividade02Pilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Stack<String> stack = new Stack<String>();
+                int seletor = 0;
+
+                do
+                {
+                    Console.WriteLine("\nInforme a Opção desejada :\n");
+                    Console.WriteLine("**********************************************\n" +
+                        "1 - ADICIONAR LIVROS NA PILHA\n" +
+                        "2 - LISTAR TODOS OS LIVROS \n" +
+                        "3 - RETIRAR LIVRO NA PILHA\n" +
+                        "0 - SAIR\n" +
+                        "*************************************************************\n" +
+                        "Entre com a Opção desejada :\n");
+
+                    seletor = Convert.ToInt32(Console.ReadLine());
+
+                    switch (seletor)
+                    {
+                        case 1:
+                            Console.WriteLine("\nInforme o nome Do Livro\n");
+                            stack.Push(Console.ReadLine());
+                            Console.WriteLine("\nLivro Adicionado\n");
+                            break;
+                        case 2:
+                            Console.WriteLine("\nLista de Livros \n");
+                            foreach (String s in stack) Console.WriteLine(s);
+                            break;
+                        case 3:
+                            if (stack.Count != 0)
+                            {
+                                Console.WriteLine("\nO Livro foi removido\n");
+                                stack.Pop();
+                            }
+                            else
+                                Console.WriteLine("A Pilha esta Vazia");
+                            break;
+                        case 0:
+                            Console.WriteLine("\nObrigado\n");
+                            break;
+                        default:
+                            Console.WriteLine("\nUnknown value\n");
+                            break;
+                    }
+
+                } while (seletor != 0);
+            }
+        }
+    }
+}
